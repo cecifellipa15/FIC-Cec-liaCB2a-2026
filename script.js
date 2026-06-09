c2e3df5875be8cd2ef809b8ef61a14de8f58fddf
cecifellipa15 <cecifellipa@gmail.com>
2026-06-09 08:29:45 -0300

    Refactor adicionar and analisar functions with age average

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 let pessoas = []
 
+let votosA = 0
+let votosB = 0 
+let votosC = 0
+
 function adicionar() {
 
 let nome = document.getElementById("nome").value
 let idade = Number(document.getElementById("idade").value)
 
-
-if (nome == "" || idade <=0){
+if (nome === "" || idade <=0){
     
 alert("Por favor preencha todos os campos corretamente")
 return
 }
-
-console.log(nome)
 
 pessoas.push({nome: nome, idade: idade})
 
@@ -25,7 +26,6 @@ document.getElementById('nome').value = ""
 document.getElementById('idade').value = ""
 }
 
-
 function analisar() {
     
     if (pessoas.length === 0){
@@ -34,31 +34,37 @@ function analisar() {
     }
 
 let resultado = ""
+let aptos = 0
+let somaIdades = 0
 for (let i = 0; i < pessoas.length; i++){
 
     let nome = pessoas[i].nome
 let idade = pessoas[i].idade
+
+somaIdades += idade
+
+
 
 if (idade < 16){
 resultado += nome + " - Não pode votar <br>"
 }
 
 else if (idade < 18 || idade >= 70){
-resultado += nome + " - Voto facultativo <br>"}
+resultado += nome + " - Voto facultativo <br>"
+aptos++
+}
 
 else{
-resultado += nome + " - Voto obrigatório <br>"}
-
+resultado += nome + " - Voto obrigatório <br>"
+aptos++
+}
 }
     
+let media = somaIdades / pessoas.length
+
+resultado += "br><strong>Media das idades: " + media.toFixed(1) + "anos</strong>"
+
 
 document.getElementById("resultado").innerHTML = resultado
+
 }
-
-
-
-
-
-
-
-
